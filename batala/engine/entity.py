a423123a2c0da9fc1f650b126d124ac0a54b4bc2
Brian Lambrigger <lambrigger.brian@gmail.com>
2023-10-10 20:25:29 -0500

    Add docstrings to Entity and EntityManager

--- batala/engine/entity.py
+++ batala/engine/entity.py
@@ -5,9 +5,20 @@ from batala.engine import GENERATION_MASK, INDEX_BITS, INDEX_MASK, Generation, I
 
 @dataclass
 class Entity():
+    """An Entity is a simple dataclass that wraps an Id and exposes some
+    utility methods. Id, Index, and Generation are typealiases defined in
+    batala.engine.__init__.py
+    """
     _id: Id
 
     def __init__(self, index: Index = 0, generation: Generation = 0, id: Id = None):
+        """Initialize an Entity.
+
+        Args:
+            index (Index, optional): Index component of Id. Defaults to 0.
+            generation (Generation, optional): Generation component of Id. Defaults to 0.
+            id (Id, optional): Set Id to this value instead of generating. Defaults to None.
+        """
         if id is not None:
             self._id = id
         else:
@@ -15,30 +26,92 @@ class Entity():
 
     @property
     def id(self) -> Id:
+        """Get Id of the Entity.
+
+        Returns:
+            Id: the entity Id value (typealias Id defined in batala.engine.__init__.py)
+        """
         return self._id
 
     @property
     def index(self) -> Index:
+        """Get Index of the Entity from its Id.
+
+        Returns:
+            Index: the Index value of the entity Id (typealias Index defined in batala.engine.__init__.py)
+        """
         return self.get_index(self._id)
 
     @property
     def generation(self) -> Generation:
+        """Get Generation of the Entity from its Id.
+
+        Returns:
+            Generation: the Generation value of the entity Id (typealias Generation defined in batala.engine.__init__.py)
+        """
         return self.get_generation(self._id)
 
     @staticmethod
     def generate_id(index: Index = 0, generation: Generation = 0) -> Id:
+        """Get an Id from an Index value and a Generation value.
+
+        First INDEX_BITS bits is the Index value, last GENERATION_BITS bits is
+        Generation value.
+
+        Args:
+            index (Index, optional): Index value of the Id. Defaults to 0.
+            generation (Generation, optional): Generation value of the Id. Defaults to 0.
+
+        Returns:
+            Id: the combined Id value (typealias Id defined in batala.engine.__init__.py)
+        """
         return bitwise_or(index, left_shift(generation, INDEX_BITS))
 
     @staticmethod
     def get_index(id: Id) -> Index:
+        """Get an Index from an Id
+        First INDEX_BITS bits is the Index value, so bitwise_and with a mask
+        gives us the value.
+
+        Args:
+            id (Id): the Id to query
+
+        Returns:
+            Index: the Index value of the Id
+        """
         return bitwise_and(id, INDEX_MASK)
 
     @staticmethod
     def get_generation(id: Id) -> Generation:
+        """Get a Generation from an Id
+        Last GENERATION_BITS bits is the generation value, so right_shift then
+        bitwise_and with mask gives us the value.
+
+        Args:
+            id (Id): the Id to query
+
+        Returns:
+            Generation: the Generation value of the Id
+        """
         return bitwise_and(right_shift(id, INDEX_BITS), GENERATION_MASK)
 
     def __hash__(self) -> int:
+        """Return hash (just the _id attribute)
+
+        Returns:
+            int: _description_
+        """
         return self._id
 
     def __eq__(self, __value: object) -> bool:
+        """Equals operator.
+
+        Checks if __value is Entity, then compares _id values.
+
+        Args:
+            __value (object): object to compare, if not Entity will always be False
+
+        Returns:
+            bool: True if __value is Entity and _id values are equal
+        """
         return isinstance(__value, Entity) and __value._id == self._id
--- batala/engine/entity_manager.py
+++ batala/engine/entity_manager.py
@@ -5,16 +5,31 @@ from numpy import ndarray, zeros
 
 
 class EntityManager():
+    """An EntityManager creates and monitors Entities
+    by maintaining an array of Generation values.
+    """
     _entities: ndarray
     _free_indices: deque[Index]
     _back: int = 0
     _count: int = 0
 
     def __init__(self):
+        """Initializes an empty EntityManager with MAX_ENTITIES capacity.
+
+        MAX_ENTITIES is defined in batala.engine.__init__.py
+        """
         self._entities = zeros(MAX_ENTITIES, dtype=Generation)
         self._free_indices = deque()
 
     def create(self) -> Entity:
+        """Create an Entity.
+
+        The entity Id will have an Index and Generation value according to the 
+        EntityManager state.
+
+        Returns:
+            Entity: the created entity
+        """
         index = Index(self._back)
         if len(self._free_indices) > MINIMUM_FREE_INDICES:
             index = Index(self._free_indices.popleft())
@@ -25,14 +40,40 @@ class EntityManager():
         return Entity(index, generation)
 
     def is_alive(self, entity: Entity) -> bool:
+        """Check if an Entity is alive.
+
+        An Entity is alive if its Generation value is equal to the Generation
+        value at the Index of the EntityManager.
+
+        Args:
+            entity (Entity): the Entity to query with
+
+        Returns:
+            bool: True if the Entity is alive, else False
+        """
         return self._entities[entity.index] == entity.generation
 
     def destroy(self, entity: Entity):
+        """Destroy an Entity.
+
+        Increment the Generation value at Entity's Index, add the Index to the
+        _free_indices queue for recycling, and decrement _count.
+
+        Args:
+            entity (Entity): the Entity to destroy
+        """
+        if not self.is_alive(entity):
+            return
         index = entity.index
         self._entities[index] += 1
         self._free_indices.append(index)
         self._count -= 1
 
     @property
-    def count(self):
+    def count(self) -> int:
+        """Get the count of living Entities.
+
+        Returns:
+            int: the number of living Entities
+        """
         return self._count
